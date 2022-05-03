86154ed31672ccb9ec9b04cb7a113613b6f41a19
RodrigoDEVS <rodrigo.gallego@outlook.es>
2022-05-03 07:27:51 -0500

    Agregada Tabla: como otra opción se agrega tabla con información de cabecera para los TODO

--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,22 +1,36 @@
 import React from 'react';
 import './TodoItem.css';
-import { Form, Button } from 'react-bootstrap'
+import { Form, Button, Table } from 'react-bootstrap'
 
 function TodoItem(props) {
   return (
     <div className='container'>
       <li className="TodoItem">
-      <Form.Group className="mb-3" controlId="formBasicCheckbox" onClick={props.onComplete}>
-        <Form.Check type="checkbox" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Control type="text" disabled defaultValue={props.text} />
-      </Form.Group>
-
-      <Button className="mb-3" onClick={props.onDelete}>Eliminar</Button>
-    </li>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Hecho</th>
+              <th>Tarea</th>
+              <th>Eliminar</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox" onClick={props.onComplete}>
+                  <Form.Check type="checkbox" />
+                </Form.Group>
+              </td>
+              <td>{props.text}</td>
+              <td>
+                <Button className="mb-3" onClick={props.onDelete}>Eliminar</Button>
+              </td>
+            </tr>
+          </tbody>
+        </Table>
+      </li>
     </div>
-    
+
   );
 }
 
