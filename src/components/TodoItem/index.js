0278489077234b7d227f36a71c41dc842167ebad
RodrigoDEVS <rodrigo.gallego@outlook.es>
2022-05-03 05:31:09 -0500

    Boton Eliminar: Se cambia la 'X' por un botón para eliminar el ToDo

--- src/components/TodoItem/index.js
+++ src/components/TodoItem/index.js
@@ -1,28 +1,17 @@
 import React from 'react';
 import './TodoItem.css';
-import { Form } from 'react-bootstrap'
+import { Form, Button } from 'react-bootstrap'
 
 function TodoItem(props) {
   return (
     <li className="TodoItem">
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
+      <Form.Group className="mb-3" controlId="formBasicCheckbox" onClick={props.onComplete}>
+        <Form.Check type="checkbox" />
       </Form.Group>
-      <span
-        className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
-        onClick={props.onComplete}
-      >
-        √
-      </span>
       <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
         {props.text}
       </p>
-      <span
-        className="Icon Icon-delete"
-        onClick={props.onDelete}
-      >
-        X
-      </span>
+      <Button onClick={props.onDelete}>Eliminar</Button>
     </li>
   );
 }
